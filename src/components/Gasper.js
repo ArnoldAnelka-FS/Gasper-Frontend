276fc9b58960e6e2ae2295756e7be87a53624511
Anelka <aaarnold@student.fullsail.edu>
2024-05-02 19:58:22 -0400

    Deploy Frontend

---
+++ src/components/Gasper.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Gasper = () => {
+  return (
+    <div className="background">
+      <div className="gasper-container">
+        <div className="gasper-word">Gasper</div>
+        <div className="moving-line"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Gasper;
---
+++ src/components/Homepage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Button } from 'react-bootstrap';
+import SignupForm from './SignupForm';
+
+
+ 
+const Homepage = () => {
+  return (
+    <Container fluid>
+      <SignupForm/>
+    
+    </Container>
+  );
+};
+
+
+export default Homepage;
